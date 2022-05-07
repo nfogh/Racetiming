512089b73aa6e5feb854552081a75c3dfc78640f
Nikolaj Fogh <nikolajfogh@gmail.com>
2022-05-08 01:14:18 +0200

    Initiaize database

--- data/initdb.sql
+++ data/initdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: May 06, 2022 at 03:13 PM
+-- Generation Time: May 07, 2022 at 10:57 PM
 -- Server version: 8.0.29
 -- PHP Version: 8.0.15
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `events`
 --
 
@@ -32,7 +45,8 @@ CREATE TABLE `events` (
   `runnerid` int NOT NULL,
   `timestamp` timestamp NOT NULL,
   `msecs` int NOT NULL,
-  `event` int NOT NULL
+  `event` int NOT NULL,
+  `id` bigint NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -44,7 +58,20 @@ CREATE TABLE `events` (
 CREATE TABLE `numbers` (
   `raceid` int NOT NULL,
   `runnerid` int NOT NULL,
-  `number` int NOT NULL
+  `number` int NOT NULL,
+  `id` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int NOT NULL,
+  `adminid` int NOT NULL,
+  `raceid` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -55,7 +82,24 @@ CREATE TABLE `numbers` (
 
 CREATE TABLE `races` (
   `id` int NOT NULL,
-  `description` text NOT NULL
+  `description` text NOT NULL,
+  `name` text NOT NULL,
+  `start` datetime DEFAULT NULL,
+  `address` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `gpscoords` point DEFAULT NULL,
+  `finish` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rest_api_keys`
+--
+
+CREATE TABLE `rest_api_keys` (
+  `adminid` int NOT NULL,
+  `api_key` varchar(128) NOT NULL,
+  `id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -85,22 +129,99 @@ CREATE TABLE `runners` (
 --
 
 --
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`name`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `numbers`
+--
+ALTER TABLE `numbers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `races`
 --
 ALTER TABLE `races`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `rest_api_keys`
+--
+ALTER TABLE `rest_api_keys`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `rfidtags`
 --
 ALTER TABLE `rfidtags`
-  ADD PRIMARY KEY (`runnerid`);
+  ADD PRIMARY KEY (`tid`);
 
 --
 -- Indexes for table `runners`
 --
 ALTER TABLE `runners`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `numbers`
+--
+ALTER TABLE `numbers`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `races`
+--
+ALTER TABLE `races`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `rest_api_keys`
+--
+ALTER TABLE `rest_api_keys`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `runners`
+--
+ALTER TABLE `runners`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
